cf47b78ffb5e73e355043ada3f4fbe875111e8ba
potestad29@gmail.com <potestad29@gmail.com>
2020-03-21 15:42:37 +0800

    final navbar

--- FLCwebApp/Mainpage.aspx.designer.cs
+++ FLCwebApp/Mainpage.aspx.designer.cs
@@ -85,14 +85,5 @@ namespace FLCwebApp
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton logoutbtn;
-
-        /// <summary>
-        /// Label2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
